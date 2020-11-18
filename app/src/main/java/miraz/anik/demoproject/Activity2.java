fe00bfa21280a1dfbb7f8424b026eeafa105ba38
Miraz Anik <miraz.anik@gmail.com>
2020-11-19 03:03:56 +0600

    up to quantity

--- app/src/main/java/miraz/anik/demoproject/Activity2.java
+++ app/src/main/java/miraz/anik/demoproject/Activity2.java
@@ -3,19 +3,32 @@ package miraz.anik.demoproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
 
     Toast toast;
+    ImageView plus;
+    ImageView minus;
+    TextView count;
+
+    int counter = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.slide_up_activity);
-        if (toast != null){
-            toast.cancel();
-        }
-        toast = Toast.makeText(Activity2.this, "Working with Backend..", Toast.LENGTH_SHORT);
-        toast.show();
+
+        plus = findViewById(R.id.plus);
+        minus = findViewById(R.id.minus);
+        count = findViewById(R.id.count);
+        plus.setOnClickListener(view -> count.setText(" " + ++counter));
+        minus.setOnClickListener(view -> {
+            if (counter > 0) count.setText(" " + --counter);
+        });
+
     }
 }
