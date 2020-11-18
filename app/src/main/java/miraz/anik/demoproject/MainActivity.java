667e6e102f3df431ea370a184dbf33b0f738cd08
Miraz Anik <miraz.anik@gmail.com>
2020-11-18 22:46:14 +0600

    Initial commit

---
+++ app/src/main/java/miraz/anik/demoproject/MainActivity.java
@@ -0,0 +1,13 @@
+package miraz.anik.demoproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
