f3b12e42c8450ddb1d05004181ad0d8ad20ae6b8
Miraz Anik <miraz.anik@gmail.com>
2020-11-19 02:51:00 +0600

    70% backend

--- app/src/main/java/miraz/anik/demoproject/MainActivity.java
+++ app/src/main/java/miraz/anik/demoproject/MainActivity.java
@@ -2,9 +2,11 @@ package miraz.anik.demoproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView someTextView = (TextView) findViewById(R.id.tvsot);
+        someTextView.setText("BDT. 3,050");
+        someTextView.setPaintFlags(someTextView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+
         LinearLayout app_layer = (LinearLayout) findViewById(R.id.click1);
 
 
