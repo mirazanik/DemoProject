e35d010ab7fac9d44675b23f037b527350dc2619
Miraz Anik <miraz.anik@gmail.com>
2020-11-19 02:39:32 +0600

    60% backend

--- app/src/main/java/miraz/anik/demoproject/MainActivity.java
+++ app/src/main/java/miraz/anik/demoproject/MainActivity.java
@@ -1,14 +1,32 @@
 package miraz.anik.demoproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.slide_up_activity);
+        setContentView(R.layout.activity_main);
+        LinearLayout app_layer = (LinearLayout) findViewById(R.id.click1);
+
+
+        app_layer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Activity2.class);
+                startActivity(intent);
+                overridePendingTransition(R.transition.slide_in, R.transition.slide_out);
+            }
+        });
+
 
     }
 }
