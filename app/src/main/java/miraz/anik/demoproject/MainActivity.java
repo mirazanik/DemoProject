ec3fd55dcb3fd2f40b7ed2f5b1d924f1ba98b220
Miraz Anik <miraz.anik@gmail.com>
2020-11-18 22:50:19 +0600

    migrate AndroidX

--- app/src/main/java/miraz/anik/demoproject/MainActivity.java
+++ app/src/main/java/miraz/anik/demoproject/MainActivity.java
@@ -1,6 +1,6 @@
 package miraz.anik.demoproject;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
